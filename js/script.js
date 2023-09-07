61666a7de1d31f52052fd63fc682ce1e2cd2e81a
mothies-m <mothieswaran711@gmail.com>
2023-09-07 20:58:11 +0530

    Made changes in script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const time = document.querySelector(".time"),
     name = document.querySelector(".name"),
+    wish = document.querySelector(".wish");
     todayFocus = document.querySelector(".todayFocus"),
     focus = document.querySelector(".focus");
 
@@ -22,4 +23,26 @@ function addZero(n){
     return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
-showTime(); 
+function setBgImage(){
+    let today = new Date(),
+        hour = today.getHours();
+    
+    if(hour < 12){
+        document.body.style.backgroundImage = "url('../image/morning.jpg')";
+        wish.textContent = "Good Morning";
+    }
+    else if(hour < 18){
+        document.body.style.backgroundImage = "url('../image/evening.jpg')";
+        wish.textContent = "Good Evening";
+    }
+    else{
+        document.body.style.backgroundImage = "url('../image/night.jpg')";
+        wish.textContent = "Good Night";
+        document.body.style.color = "white";
+    }
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+}
+
+showTime();
+setBgImage();
